17e6c1a8463c6e1196a1efc6b8938ef7a414611b
Mihael <mvmihov@gmail.com>
2023-01-26 12:15:23 +0200

    Add json property annotation

--- src/main/java/com/example/avroandkafka/avroandkafkademo/model/Order.java
+++ src/main/java/com/example/avroandkafka/avroandkafkademo/model/Order.java
@@ -1,6 +1,8 @@
 package com.example.avroandkafka.avroandkafkademo.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Order {
+
+    @JsonProperty
     private int orderNum;
+    @JsonProperty
     private String orderItem;
+    @JsonProperty
     private int price;
 }
